bf03390f0ae86dd7871b661216e6966b932e932c
Sammuel Reis <sammugr@hotmail.com>
2021-12-13 23:05:38 -0300

    feat: creates TransactionsContext

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
-import { FormEvent, useState } from "react";
+import { FormEvent, useContext, useState } from "react";
 import Modal from "react-modal";
 
-import { api } from "../../services/api";
+import { TransactionsContext } from "../../TransactionsContext";
 
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
@@ -19,21 +19,21 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  const { createTransaction } = useContext(TransactionsContext);
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = {
+    createTransaction({
       title,
-      value,
+      amount,
       category,
       type,
-    };
-
-    api.post("transactions", data);
+    });
   }
 
   return (
@@ -61,8 +61,8 @@ export function NewTransactionModal({
         />
 
         <input
-          value={value}
-          onChange={(event) => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={(event) => setAmount(Number(event.target.value))}
           type="number"
           placeholder="Valor"
         />
